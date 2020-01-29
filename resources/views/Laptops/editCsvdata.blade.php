0d31e2ff5c8e1edc43bcfca6c83d48675733a9f5
Intel.grs.lab.1 <intel.grs.lab.1@gmail.com>
2020-01-29 13:46:51 +0000

    edit and insert one changes compete

--- resources/views/Laptops/editCsvdata.blade.php
+++ resources/views/Laptops/editCsvdata.blade.php
@@ -17,10 +17,14 @@
     @endif
         <form method="POST" name="updateCsvData" action="{{ url('import/updatedata', $laptop->id) }}">
         @csrf
-<form method="POST" name="updateCsvData" action="{{ url('import/insertdata') }}">
+        <form method="POST" name="updateCsvData" action="{{ url('import/insertdata') }}">
         @csrf
-                <div class="form-group">
-          <label for="name">Computer Brand Name</label>
+        <div class="form-group">
+          <label for="name">Manufacture</label>
+          <input type="text" class="form-control" name="manufacture" value= "{{@$laptop['manufacture'] }}"/>
+        </div>
+        <div class="form-group">
+          <label for="name">Model</label>
           <input type="text" class="form-control" name="com_brand_name" value= "{{@$laptop['com_brand_name'] }}"/>
         </div>
         <div class="form-group">
@@ -28,10 +32,21 @@
           <textarea type="text" class="form-control" name="colour">{{@$laptop['colour'] }}</textarea>
         </div>
         <div class="form-group">
-          <label for="name">Power supply</label>
-          <textarea type="text" class="form-control" name="power_supply">{{@$laptop['power_supply'] }}</textarea>
+          <label for="name">Type</label>
+          <textarea type="text" class="form-control" name="type">{{@$laptop['type'] }}</textarea>
         </div>
-
+        <div class="form-group">
+          <label for="name">Battery capacity</label>
+          <textarea type="text" class="form-control" name="battery_cap">{{@$laptop['battery_cap'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Power supply</label>
+          <textarea type="text" class="form-control" name="power_supply_details">{{@$laptop['power_supply_details'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">power supply details</label>
+          <textarea type="text" class="form-control" name="colour">{{@$laptop['colour'] }}</textarea>
+        </div>
         <div class="form-group">
           <label for="name">Operating System</label>
           <textarea type="text" class="form-control" name="os">{{@$laptop['os'] }}</textarea>
@@ -43,33 +58,68 @@
         </div>
 
         <div class="form-group">
-          <label for="name">CPU Power Limit</label>
+          <label for="name">CPU Power Limit 1</label>
           <input type="text" class="form-control" name="cpu_power_limit" value="{{@$laptop['cpu_power_limit'] }}"/>
         </div>
-
+        <div class="form-group">
+          <label for="name">CPU Power Limit 2</label>
+          <input type="text" class="form-control" name="cpu_power_limit_2" value="{{@$laptop['cpu_power_limit_2'] }}"/>
+        </div>
         <div class="form-group">
           <label for="name">Total Memory Size</label>
           <input type="text" class="form-control" name="total_mem_size" value="{{@$laptop['total_mem_size'] }}"/>
         </div>
-
+        <div class="form-group">
+          <label for="name">Memory type</label>
+          <textarea rows="4" type="text" class="form-control" name="mem_type">{{@$laptop['mem_type'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">memory speed</label>
+          <textarea rows="4" type="text" class="form-control" name="mem_speed">{{@$laptop['mem_speed'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Memory channel</label>
+          <textarea rows="4" type="text" class="form-control" name="mem_channels">{{@$laptop['mem_channels'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">screen size</label>
+          <textarea rows="4" type="text" class="form-control" name="screen_size">{{@$laptop['screen_size'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">resolution</label>
+          <textarea rows="4" type="text" class="form-control" name="screen_rez">{{@$laptop['screen_rez'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Screen Technology</label>
+          <textarea rows="4" type="text" class="form-control" name="screen_tech">{{@$laptop['screen_tech'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Touchscreen type</label>
+          <textarea rows="4" type="text" class="form-control" name="touchscreen_type">{{@$laptop['touchscreen_type'] }}</textarea>
+        </div>
         <div class="form-group">
           <label for="name">Dive Capacity</label>
           <textarea type="text" class="form-control" name="drive_capacity">{{@$laptop['drive_capacity'] }}</textarea>
         </div>
-
         <div class="form-group">
-          <label for="name">Computer Serial Number</label>
+          <label for="name">Serial Number</label>
           <input type="text" class="form-control" name="com_serial_number" value="{{@$laptop['com_serial_number'] }}"/>
         </div>
-
         <div class="form-group">
           <label for="name">Video Card</label>
           <textarea rows="4" type="text" class="form-control" name="videocard">{{@$laptop['videocard'] }}</textarea>
         </div>
-
         <div class="form-group">
           <label for="name">Network</label>
           <textarea rows="4" type="text" class="form-control" name="network">{{@$laptop['network'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">No. thunderbolt ports</label>
+          <textarea rows="4" type="text" class="form-control" name="thunderbolt_ports">{{@$laptop['thunderbolt_ports'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">accessories</label>
+          <textarea rows="4" type="text" class="form-control" name="accessories">{{@$laptop['accessories'] }}</textarea>
         </div>
         <div class="form-group">
           <label for="name">owner</label>
--- resources/views/Laptops/insertCsv.blade.php
+++ resources/views/Laptops/insertCsv.blade.php
@@ -16,66 +16,119 @@
         <form method="POST" name="updateCsvData" action="{{ url('import/insertdata') }}">
         @csrf
 
-         <div class="form-group">
-          <label for="name">Computer Brand Name</label>
-          <input type="text" class="form-control" name="com_brand_name" value= "{{$laptop['com_brand_name'] }}"/>
+        <div class="form-group">
+          <label for="name">Manufacture</label>
+          <input type="text" class="form-control" name="manufacture" value= "{{@$laptop['manufacture'] }}"/>
         </div>
-
+        <div class="form-group">
+          <label for="name">Model</label>
+          <input type="text" class="form-control" name="com_brand_name" value= "{{@$laptop['com_brand_name'] }}"/>
+        </div>
         <div class="form-group">
           <label for="name">Colour</label>
-          <textarea type="text" class="form-control" name="colour">{{$laptop['colour'] }}</textarea>
+          <textarea type="text" class="form-control" name="colour">{{@$laptop['colour'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Type</label>
+          <textarea type="text" class="form-control" name="type">{{@$laptop['type'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Battery capacity</label>
+          <textarea type="text" class="form-control" name="battery_cap">{{@$laptop['battery_cap'] }}</textarea>
         </div>
         <div class="form-group">
           <label for="name">Power supply</label>
-          <textarea type="text" class="form-control" name="power_supply">{{$laptop['power_supply'] }}</textarea>
+          <textarea type="text" class="form-control" name="power_supply_details">{{@$laptop['power_supply_details'] }}</textarea>
         </div>
-
+        <div class="form-group">
+          <label for="name">power supply details</label>
+          <textarea type="text" class="form-control" name="colour">{{@$laptop['colour'] }}</textarea>
+        </div>
         <div class="form-group">
           <label for="name">Operating System</label>
-          <textarea type="text" class="form-control" name="os">{{$laptop['os'] }}</textarea>
+          <textarea type="text" class="form-control" name="os">{{@$laptop['os'] }}</textarea>
         </div>
 
         <div class="form-group">
           <label for="name">CPU Brand Name</label>
-          <textarea type="text" class="form-control" name="cpu_brand_name">{{$laptop['cpu_brand_name'] }}</textarea>
+          <textarea type="text" class="form-control" name="cpu_brand_name">{{@$laptop['cpu_brand_name'] }}</textarea>
         </div>
-
 
         <div class="form-group">
-          <label for="name">CPU Power Limit</label>
-          <input type="text" class="form-control" name="cpu_power_limit" value="{{$laptop['cpu_power_limit'] }}"/>
+          <label for="name">CPU Power Limit 1</label>
+          <input type="text" class="form-control" name="cpu_power_limit" value="{{@$laptop['cpu_power_limit'] }}"/>
         </div>
-
+        <div class="form-group">
+          <label for="name">CPU Power Limit 2</label>
+          <input type="text" class="form-control" name="cpu_power_limit_2" value="{{@$laptop['cpu_power_limit_2'] }}"/>
+        </div>
         <div class="form-group">
           <label for="name">Total Memory Size</label>
-          <input type="text" class="form-control" name="total_mem_size" value="{{$laptop['total_mem_size'] }}"/>
+          <input type="text" class="form-control" name="total_mem_size" value="{{@$laptop['total_mem_size'] }}"/>
         </div>
-
         <div class="form-group">
-          <label for="name">Computer Serial Number</label>
-          <input type="text" class="form-control" name="com_serial_number" value="{{$laptop['com_serial_number'] }}"/>
+          <label for="name">Memory type</label>
+          <textarea rows="4" type="text" class="form-control" name="mem_type">{{@$laptop['mem_type'] }}</textarea>
         </div>
-        
+        <div class="form-group">
+          <label for="name">memory speed</label>
+          <textarea rows="4" type="text" class="form-control" name="mem_speed">{{@$laptop['mem_speed'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Memory channel</label>
+          <textarea rows="4" type="text" class="form-control" name="mem_channels">{{@$laptop['mem_channels'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">screen size</label>
+          <textarea rows="4" type="text" class="form-control" name="screen_size">{{@$laptop['screen_size'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">resolution</label>
+          <textarea rows="4" type="text" class="form-control" name="screen_rez">{{@$laptop['screen_rez'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Screen Technology</label>
+          <textarea rows="4" type="text" class="form-control" name="screen_tech">{{@$laptop['screen_tech'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Touchscreen type</label>
+          <textarea rows="4" type="text" class="form-control" name="touchscreen_type">{{@$laptop['touchscreen_type'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Dive Capacity</label>
+          <textarea type="text" class="form-control" name="drive_capacity">{{@$laptop['drive_capacity'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Serial Number</label>
+          <input type="text" class="form-control" name="com_serial_number" value="{{@$laptop['com_serial_number'] }}"/>
+        </div>
         <div class="form-group">
           <label for="name">Video Card</label>
-          <textarea rows="4" type="text" class="form-control" name="videocard">{{$laptop['videocard'] }}</textarea>
+          <textarea rows="4" type="text" class="form-control" name="videocard">{{@$laptop['videocard'] }}</textarea>
         </div>
-
         <div class="form-group">
           <label for="name">Network</label>
-          <textarea rows="4" type="text" class="form-control" name="network">{{$laptop['network'] }}</textarea>
+          <textarea rows="4" type="text" class="form-control" name="network">{{@$laptop['network'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">No. thunderbolt ports</label>
+          <textarea rows="4" type="text" class="form-control" name="thunderbolt_ports">{{@$laptop['thunderbolt_ports'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">accessories</label>
+          <textarea rows="4" type="text" class="form-control" name="accessories">{{@$laptop['accessories'] }}</textarea>
         </div>
         <div class="form-group">
           <label for="name">owner</label>
-          <textarea rows="4" type="text" class="form-control" name="owner">{{$laptop['owner'] }}</textarea>
-        </div>
-        <div class="form-group">
-          <label for="name">Comments</label>
-          <textarea type="text" class="form-control" name="comments">{{$laptop['comments'] }}</textarea>
+          <textarea rows="4" type="text" class="form-control" name="owner">{{@$laptop['owner'] }}</textarea>
         </div>
         <div class="form-group">
           <label for="name">Location</label>
-          <textarea type="text" class="form-control" name="location">{{$laptop['location'] }}</textarea>
+          <textarea type="text" class="form-control" name="location">{{@$laptop['location'] }}</textarea>
+        </div>
+        <div class="form-group">
+          <label for="name">Comments</label>
+          <textarea type="text" class="form-control" name="comments">{{@$laptop['comments'] }}</textarea>
         </div>
         <!-- Insert button -->
         <button type="submit" class="btn btn-primary">Insert</button>
