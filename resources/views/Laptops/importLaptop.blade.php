37a3c21255b6f86bb73b43459ca90df2e34a5708
intel-grs-lab-1 <intel.grs.lab.1@gmail.com>
2020-02-14 14:03:38 +0000

    inline colour and small format changes

--- resources/views/Laptops/importLaptop.blade.php
+++ resources/views/Laptops/importLaptop.blade.php
@@ -8,6 +8,7 @@
         Import CSV
         </div>
         <div class="card-body">
+            <h2 style="color:red;" >DO NOT IMPORT PRE-RELEASE PRODUCTS!</h2>
             <form action="{{ url('import') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <input type="file" name="csvfile" accept=".csv, .txt" class="form-control">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,15 +66,15 @@
 
             <li class="nav-item dropdown">
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-               Import <span class="caret"></span>
+               Laptops <span class="caret"></span>
              </a>
 
              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="{{ url('/viewall') }}">
-                View All
+                View All Laptops
               </a>
               <a class="dropdown-item" href="{{ url('/view/import') }}">
-               Import
+               HWInfo Import
              </a>
            </div>
 
